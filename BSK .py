cdbd81b39a7999073a3106b8d03ba2e79edc1c41
MinamiUZ-1984 <yuji.minami@y-yokohama.com>
2026-04-01 08:16:53 +0900

    Update BSK .py

--- BSK .py
+++ BSK .py
@@ -94,7 +94,7 @@ if 'read_only' not in st.session_state:
 
 # --- 使用者名ログイン＆ロック画面 ---
 if 'room_key' not in st.session_state:
-    st.title("🏀 松浪ミニバス 🏀スタッツ分析")
+    st.title("🏀 松浪ミニバス 🏀 スタッツ分析")
     st.info("💡 **使用者名** を入力してスタートしてください。")
     room_input = st.text_input("使用者名（例：山田花子 など）")
     col1, col2 = st.columns(2)
